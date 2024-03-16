5ff0567bbb90859203df63865f1b650a88b4d123
Professora Luciene Cavalcanti <lucienecavalcanti@live.com>
2024-03-16 14:49:22 -0300

    Atualização do APP2 com os itens do menu

--- src/App2.java
+++ src/App2.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
+import model.Estoque;
 import model.NaoPerecivel;
 import model.Perecivel;
 
@@ -17,7 +22,10 @@ public class App2 {
                 "\n2 - Cadastrar não perecível"+
                 "\n3 - Listar Perecíveis"+
                 "\n4 - Listar não perecíveis"+
-                "\n5 - Sair");
+                "\n5 - Procurar por descrição"+
+                "\n6 - Mostrar os 3 perecíveis mais baratos"+
+                "\n7 - Mostrar os 3 produtos mais baratos (entre Perecíveis e não perecíveis)"+
+                "\n10 - Sair");
                 opcao = entrada.nextInt();
                 switch (opcao) {
                     case 1: 
@@ -41,19 +49,82 @@ public class App2 {
                         System.out.println("------ Não Perecíveis -----");
                         for (NaoPerecivel np : listaNaoPereciveis) {
                             np.mostrar();
-                            System.out.println(np);
+                            //ou System.out.println(np); //toString()
                         }
                         break; 
-                    case 5: break;                       
+                    case 5:
+                        System.out.println("Digite a descrição do produto");
+                        String descricao = entrada.next();
+                        for (NaoPerecivel np : listaNaoPereciveis) {
+                            if(np.getDescricao().equalsIgnoreCase(descricao))
+                                np.mostrar();
+                        }
+                        for (Perecivel p : listaPereciveis) {
+                            if(p.getDescricao().equalsIgnoreCase(descricao))
+                                p.mostrar();
+                        }
+                        break;
+                    case 6:
+                        int contador=0;
+                        /* para não ter que digitar ao fazer os testes 
+                        listaNaoPereciveis.add(new NaoPerecivel(1, "Bolo1", 9.99f, "prateira 1"));
+                        listaNaoPereciveis.add(new NaoPerecivel(2, "Bolo2", 7.99f, "prateira 1"));
+                        listaNaoPereciveis.add(new NaoPerecivel(3, "Bolo3", 5.99f, "prateira 1"));
+                        listaPereciveis.add(new Perecivel(4, "Bolo4", 4.99f, "10/10/2024"));
+                        listaPereciveis.add(new Perecivel(5, "Bolo5", 2.99f, "10/10/2024"));
+                        listaPereciveis.add(new Perecivel(6, "Bolo6", 14.99f, "10/10/2024"));
+                        listaPereciveis.add(new Perecivel(7, "Bolo7", 13.99f, "10/10/2024"));
+                        listaPereciveis.add(new Perecivel(8, "Bolo8", 6.99f, "10/10/2024"));
+                        listaPereciveis.add(new Perecivel(9, "Bolo9", 90.99f, "10/10/2024"));
+                        */
+                        for(int i=0;i<listaPereciveis.size();i++){
+                        contador=0;
+                        for(int j=0;j<listaPereciveis.size();j++){
+                            if(listaPereciveis.get(i).getPreco()<listaPereciveis.get(j).getPreco()){
+                             contador++;   
+                            }
+                        }
+                        //System.out.println("------->"+ contador);
+                        if(contador==5)
+                            listaPereciveis.get(i).mostrar();
+                        if(contador==4)
+                            listaPereciveis.get(i).mostrar();
+                        if(contador==3)
+                            listaPereciveis.get(i).mostrar();
+                        }
+                                
+                        /*   -----------   ou   ---------------
+                        Perecivel menor1 = null;
+                        Perecivel menor2 = null;
+                        Perecivel menor3 = null;
+                        for (Perecivel p : listaPereciveis) {
+                            if(menor1==null || menor1.getPreco()>p.getPreco()){
+                                menor3=menor2;
+                                menor2=menor1;
+                                menor1=p;
+                            }else if(menor2==null || (menor2.getPreco()>p.getPreco() && menor1.getPreco()<p.getPreco())){
+                                menor3=menor2;
+                                menor2=p;
+                            } else if(menor3==null || (menor3.getPreco()>p.getPreco() && menor2.getPreco()<p.getPreco())){
+                                menor3=p;
+                            }
+                        }
+                        menor1.mostrar();
+                        menor2.mostrar();
+                        menor3.mostrar();
+                        */
+                        break;
+
+                    case 10: break;                       
                     default:
                         System.out.println("Opção inválida...");
                         break;
                 }
             }catch(Exception erro){
-                System.out.println("Valor inválido!");
+                System.out.println("Erro:" + erro.getMessage());
                 entrada.next();
             }
-        } while (opcao!=5);
+        } while (opcao!=10);
 
     }
 }
